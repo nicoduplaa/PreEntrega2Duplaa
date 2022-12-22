2102ff1c398b0db554b182e882dfb42d38f9d0ec
nicoduplaa <nicolasduplaa97@gmail.com>
2022-12-22 00:25:16 -0300

    segunda-entrega

--- js/cart-test.js
+++ js/cart-test.js
@@ -1,30 +1,119 @@
-//let maceta = prompt('Ingrese el nombre de la maceta que desea comprar') ;
+//Chequeo de que la pagina cargo:
 
-let stock = ['gato','buho','dodecaedro','llama','elefante','erizo'];
-let macArr = [];
-let cantArr = [];
-let maceta = 'gato';
-let cantidad = 0;
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    console.log(removeCartItemButtons);
 
 
+    for (let i = 0; i < removeCartItemButtons.length; i++) {
+        let button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }  
 
+    let quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i]
+        input,addEventListener('change', quantityChanged)
+    }
 
-while (maceta.toLowerCase() !== 'esc') {
-    
-    maceta = prompt('Ingrese el nombre de la maceta que desea comprar, para salir ingresar ESC');
-    if (maceta.toLowerCase() == 'esc') {
-        break
-    } else if (!stock.includes(maceta.toLowerCase())) {
-        alert('No existe esa maceta en nuestro stock, recarga la página para volver a empezar')
-        break
-    } 
-    cantidad = prompt('Ingrese la cantidad que desea comprar de esa maceta');
-    macArr.push(maceta.toLowerCase());
-    cantArr.push(Number(cantidad));
+    let addToCartButtons = document.getElementsByClassName('item-button')
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchasheClicked)
+}
+
+//Comprar
+function purchasheClicked() {
+    alert('Gracias por tu compra!')
+    let cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+//REMOVER
+function removeCartItem(event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+//CANTIDAD
+function quantityChanged(event) {
+    let input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+
+    updateCartTotal()
+}
+
+//Agregar a Carrito
+function addToCartClicked(event) {
+    let button = event.target
+    let shopItem = button.parentElement.parentElement
+    let title = shopItem.getElementsByClassName('itemName')[0].innerText
+    let price = shopItem.getElementsByClassName('itemPrice')[0].innerText
+    let imageSrc = shopItem.getElementsByClassName('itemImg')[0].src
+    console.log(title, price, imageSrc);
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    let cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    let cartItems = document.getElementsByClassName('cart-items')[0]
+
+    let cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('Este producto ya fue agregado al carrito')
+            return;
+        }  
+    }
+
+    let cartRowContents = `                
+        <div class="cart-item cart-column">
+            <img class="cart-item-img" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+        cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged)
 }
 
 
-console.log(macArr);
-console.log(cantArr);
+//Actualizar Total
 
+function updateCartTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let total = 0
 
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$',''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
